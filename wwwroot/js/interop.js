86064c1ed0ca57240789d45ea6dcd91752e4d5f0
masramadotcom <63098173+masramadotcom@users.noreply.github.com>
2020-10-07 16:16:03 +0700

    Setup Jquery UI Datepicker on Blazor

---
+++ wwwroot/js/interop.js
@@ -0,0 +1,17 @@
+﻿window.JsInteropDatepicker = function () {
+    $("#datepicker").datepicker({
+        dateFormat: "dd/mm/yy",
+        minDate: 0,
+        constrainInput: false,
+        changeMonth: true,
+        changeYear: true,
+        onSelect: function (selectedDate) {
+            var myElement = document.getElementById('datepicker');
+            myElement.value = selectedDate;
+            var event = new Event('change');
+            myElement.dispatchEvent(event);
+
+            $(this).datepicker("hide");
+        },
+    });
+}
